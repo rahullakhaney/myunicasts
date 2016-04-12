5c0a7e7116b37bca4375a847838daa2255493c6f
rahullakhaney <rahul@codebrew.io>
2016-04-12 23:39:36 +0530

    Improved Player and navigation

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,4 +8,12 @@ class Lesson < ActiveRecord::Base
   validates :note, presence: true
   validates :tag, presence: true
   validates :library, presence: true
+
+  def next
+    library.lessons.where("tag > ? AND header = ?", tag, false).order(:tag).first
+  end
+
+  def prev
+    library.lessons.where("tag < ? AND header = ?", tag, false).order(:tag).last
+  end
 end
