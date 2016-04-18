6a8c50a1c9ae3742077302289cbb1f48f5424cdc
rahullakhaney <rahul@codebrew.io>
2016-04-18 12:35:21 +0530

    Added next and previous buttons

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -3,5 +3,8 @@ class LessonController < ApplicationController
     library = Library.find(params[:library_id])
     @lessons = library.lessons.order(:tag)
     @lesson = @lessons.find(params[:id])
+
+    @next_lesson = @lesson.next
+    @prev_lesson = @lesson.prev
   end
 end
